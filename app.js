d313fb885c0d5e114792c6ab9fad80d23a5e6fd6
JD Lafrance <jdavid.lafrance@gmail.com>
2016-05-19 21:38:34 +0000

    final

--- app.js
+++ app.js
@@ -1,54 +1,20 @@
-var express = require('express')
-var app = express()
-var url = require('url')
+var express = require('express');
+var multer = require('multer');
+var upload = multer({ dest: './uploads/'});
 
-var months = {
-'0': 'January',
-'1': 'February',
-'2': 'March',
-'3': 'April',
-'4': 'May',
-'5': 'June',
-'6': 'July',
-'7': 'August',
-'8': 'September',
-'9': 'October',
-'10': 'November',
-'11': 'December'
-};
-
-var obj = {
-unix: 0,
-natural: 0
-};
-
-function natural(val){return (months[val.getMonth()] + ' ' + val.getDate() + ', ' + val.getFullYear());
-};
-
-app.get('/', function(req, res){
-var url_parts = url.parse(req.url, true)                                                                    
-var search = url_parts.search;
-var str = search.replace('?','0').replace(/%20/g, ' ');
+var app = express();
+app.set('view engine', 'pug');
 
 
-if(new Date(str/1).getDate() > 0){
-obj.unix = str;
-var naturalDate = new Date(str/1);
-obj.natural = natural(naturalDate);
-res.send(obj);
-} else {
-var date = Date.parse(str);
+app.get('/', function(req, res, next){
 
-if (date){
-var naturalDate = new Date(date);
-obj.unix = date;
-obj.natural = natural(naturalDate);
-res.send(obj);
-} else {
-obj.unix = null;
-obj.natural = null;
-res.send(obj);
-}}
-})
+    res.render('index', {title: 'metadata', message: 'This gem of a software will return the size of the file you upload. Upload wisely!'});
+    
+});
+app.post('/', upload.single('fileUpload'), function (req, res) {
+    console.log(req.file);
+    res.send(500, 'File size is ' + req.file.size);
+});
+
 
 app.listen(8080);
